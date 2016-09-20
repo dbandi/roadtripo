62a58100516fbca545eda87fea18b053016f4a79
Deepak Bandi <bandideepak@gmail.com>
2016-09-19 21:34:47 -0500

    Angular Express Node Init Structure

---
+++ routes/index.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var router = express.Router();
+
+var path = require('path');
+var _ = require('underscore-node');
+var rp = require('request-promise');
+var request = require('request');
+var Promise = require("bluebird");
+
+var brandings = [];
+var directions = [];
+var totalDistance = 0;
+var totalMilesDistance = 0;
+var totalDuration = 0;
+var allCities = new Array();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
+});
+
+router.get('/plantrip', function(req, res, next) {
+
+    var request = require('request');
+    request('https://maps.googleapis.com/maps/api/directions/json?origin=Chicago&destination=Los+Angeles&key=AIzaSyBll4kPCZuJIaBsvCv_gHCRTzk5-e-8WjM', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            //directions = body; // Print the google web page.
+
+            directions = JSON.parse( response.body );
+            if(directions.status == "OK"){
+
+                totalDistance = directions.routes[0].legs[0].distance.value;
+                totalMilesDistance = totalDistance*0.000621371192;
+                totalDuration = directions.routes[0].legs[0].duration.value;
+
+                console.log(totalDistance);
+                console.log(totalMilesDistance);
+                console.log(totalDuration);
+
+                /*res.send(JSON.stringify(directions.routes[0].legs[0].steps));*/
+
+                var totalSteps = directions.routes[0].legs[0].steps.length;
+
+                var allLatLong = {};
+                /* (lenght-1) since 1st legs [steps] does not have end location */
+
+                for (var m=0; m < totalSteps - 1; m++) {
+
+                  /* All Intermediate Route Locations */
+                  allLatLong[m] = {
+                        lat : directions.routes[0].legs[0].steps[m].end_location.lat,
+                        lng : directions.routes[0].legs[0].steps[m].end_location.lng
+                    }
+                }
+
+                var allCitiesLength = Object.keys(allLatLong).length;
+
+                for (var i=0; i < allCitiesLength; i++){
+
+                    var temp=0;
+                    var city_options = {
+                        method: 'GET',
+                        uri: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + allLatLong[i].lat + ' , ' + allLatLong[i].lng + '&sensor=false&key=AIzaSyBll4kPCZuJIaBsvCv_gHCRTzk5-e-8WjM',
+                        resolveWithFullResponse: true    //  <---  <---  <---  <---
+                    };
+
+                    rp(city_options)
+                        .then(function (gresponse) {
+                            temp++;
+                            if (gresponse.statusCode == 200) {
+                                results = JSON.parse( gresponse.body );
+                                //console.log(results);
+
+                                /* Different set of address_components */
+                                for (var j = 0; j < results.results.length; j++) {
+
+                                    for (var k = 0; k < results.results[j].address_components.length; k++) {
+                                        if(results.results[j].address_components[k].types.indexOf('locality') > -1){
+                                            //console.log(results.results[j].address_components[k].long_name);
+                                            //allCities.push(results.results[j].address_components[k].long_name);
+                                            if(!(_.contains(allCities, results.results[j].address_components[k].long_name))){
+                                                allCities.push(results.results[j].address_components[k].long_name);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                if( temp == allCitiesLength){
+                                    res.json( allCities);
+                                }
+                            }
+                        })
+                        .catch(function (err) {
+                            console.log(err);
+                    });
+                }
+            }
+         }
+    });
+
+});
+
+
+module.exports = router;
